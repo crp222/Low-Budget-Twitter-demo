529f014b6e009c16f6013dd2918d82a3b2152910
Simon Tamás <h269091@stud.u-szeged.hu>
2024-03-10 22:25:50 +0100

    Revert "new updates"
    This reverts commit d4534d3fd48d4aaaf004743ec3ccd237c0ed6052.

--- backend/twitter-clone/src/main/java/com/app/twittercloneapi/Messages/Message/MessageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.twittercloneapi.Messages.Message;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MessageRepository extends CrudRepository<Message,Integer> {
-    
-}
